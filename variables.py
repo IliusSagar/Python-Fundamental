43b15828e22a54c2b94aeaf5276c6ea513ca424b
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-12-04 09:58:52 +0600

    comments

--- variables.py
+++ variables.py
@@ -1,1 +1,15 @@
 # A varicable is a container for a value, which can be of various types
+
+'''
+This is a
+multiline comment
+or docastring (used to define a functions purpose)
+can be single or double quotes
+'''
+
+"""
+VARIABLE RULES:
+    - Variable names are case sensative (name and NAME are different variables)
+    - Must start with a letter or an underscore
+    - Can have numbers but can not start with one
+"""
