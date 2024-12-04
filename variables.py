b655954b3706f23174641f2c96a9ff27079f9d33
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-12-04 10:13:37 +0600

    variables

--- variables.py
+++ variables.py
@@ -13,3 +13,23 @@ VARIABLE RULES:
     - Must start with a letter or an underscore
     - Can have numbers but can not start with one
 """
+
+# x = 1           # int
+# y = 2.5         # floar
+# name = 'Jhon'   # str
+# is_cool = True  # bool
+
+# Multiple assignment
+x, y, name, is_cool = (1, 2.5, 'Jhon', True)
+
+# Basic Math
+a = x + y
+
+# Casting
+x = str(x)
+y = int(y)
+z = float(y)
+
+# print(x, y, name, is_cool, a)
+# print(type(x))
+print(type(z), z)
