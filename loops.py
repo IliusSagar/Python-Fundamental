b73bd99adecc1da61067bc5e406cdff34be61346
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-12-05 09:25:03 +0600

    loops

---
+++ loops.py
@@ -0,0 +1,29 @@
+people = ['John', 'Paul', 'Sara', 'Susan']
+
+# Simple for loop
+# for person in people:
+#     print(f'Current Person: {person}')
+
+# Break
+# for person in people:
+#     if person == 'Sara':
+#         break
+#     print(f'Current Person: {person}')
+
+# Continue
+# for person in people:
+#     if person == 'Sara':
+#         break
+#     print(f'Current Person: {person}')
+
+# range
+# for i in range(len(people)):
+#     print(people[i])
+
+# for i in range(0, 11):
+#     print(f'Number: {i}')
+
+count = 0
+while count < 10:
+    print(f'Count: {count}')
+    count += 1
